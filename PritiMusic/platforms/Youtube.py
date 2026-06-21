8082279f6934c75f8010dd40b74485243bf6366c
THE SHIV <shivofficial004@gmail.com>
2026-06-22 02:34:55 +0530

    Update Youtube.py

--- PritiMusic/platforms/Youtube.py
+++ PritiMusic/platforms/Youtube.py
@@ -15,8 +15,8 @@ from youtubesearchpython.__future__ import VideosSearch, Playlist
 DOWNLOAD_DIR = "downloads"
 LOGGER = logging.getLogger(__name__)
 
-API_URL = os.environ.get("SHRUTI_API_URL", "https://api.pytube.fun")
-API_KEY = os.environ.get("SHRUTI_API_KEY", "HFnaZ91505yRvSZchlXeTyrpTx")
+API_URL = os.environ.get("API_URL", "https://web.riteshyt.in")
+API_KEY = os.environ.get("API_KEY", "ritesh_free_6361d74f047ed86015df3fa8")
 
 def time_to_seconds(time_str):
     stringt = str(time_str)
